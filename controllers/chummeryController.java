c8fe7a22f7be2a3e0e6768cf462fc061177be17a
AnastasiaOwl <124703588+AnastasiaOwl@users.noreply.github.com>
2023-04-19 14:46:49 +0300

    Add files via upload

---
+++ controllers/chummeryController.java
@@ -0,0 +1,87 @@
+package com.example.kursova.controllers;
+
+import com.example.kursova.dataAO.ChummeryRepository;
+import com.example.kursova.entities.Chummery;
+import com.example.kursova.entities.Payment;
+import com.example.kursova.entities.Student;
+import lombok.AllArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@org.springframework.stereotype.Controller
+@AllArgsConstructor
+public class chummeryController {
+    private ChummeryRepository chummeryRepository;
+    @GetMapping("/administration")
+    public String administration() {
+        return "administration";
+    }
+    @GetMapping("/enterChummery")
+    public String enterChummery() {
+        return "enterChummery";
+    }
+    @GetMapping("/chummery")
+    public String showChummery(Model model){
+        List<Chummery> chummery = chummeryRepository.findAll();
+        model.addAttribute("chummery",chummery);
+        return "chummery";
+    }
+    @PostMapping("/addChummery")
+    public String addPayment(@RequestParam String address,@RequestParam Integer chummeryNumber, @RequestParam Integer quRooms,@RequestParam Integer boysRNumbers, @RequestParam Integer girlsRNumbers, @RequestParam String phoneNumber){
+        Chummery chummery= new Chummery();
+        chummery.setAddress(address);
+        chummery.setChummeryNumber(chummeryNumber);
+        chummery.setQuRooms(quRooms);
+        chummery.setBoysRNumbers(boysRNumbers);
+        chummery.setGirlsRNumbers(girlsRNumbers);
+        chummery.setPhoneNumber(phoneNumber);
+        chummeryRepository.save(chummery);
+        return "redirect:/enterChummery";
+    }
+    @GetMapping("/student_chummery")
+    public String showStudentByChummery(@RequestParam int id, Model model){
+        Optional<Chummery> optionalChummery=chummeryRepository.findById(id);
+        if(optionalChummery.isPresent()){
+            model.addAttribute("chummery",optionalChummery.get());
+            return"chummery_student";
+        }
+        else{
+            return "redirect:/chummery";
+        }
+    }
+    @GetMapping("/delete_chummery")
+    public String deleteChummery(@RequestParam int id){
+       chummeryRepository.deleteById(id);
+       return "redirect:/chummery";
+    }
+    @GetMapping("/edit_chummery")
+    public String editChummery(@RequestParam int id,Model model){
+        Optional<Chummery> optionalChummery= chummeryRepository.findById(id);
+        if (optionalChummery.isEmpty()){
+            return "redirect:/chummery";
+        }
+        model.addAttribute("chummery",optionalChummery.get());
+        return "edit_chummery";
+    }
+    @PostMapping("/update_chummery")
+    public String updateChummery(@RequestParam int id,@RequestParam String address,@RequestParam Integer chummeryNumber,@RequestParam Integer quRooms, @RequestParam Integer boysRNumbers,
+                                 @RequestParam Integer girlsRNumbers, @RequestParam String phoneNumber){
+        Optional<Chummery> optionalChummery= chummeryRepository.findById(id);
+        optionalChummery.ifPresent(chummery -> {
+        chummery.setAddress(address);
+        chummery.setChummeryNumber(chummeryNumber);
+        chummery.setQuRooms(quRooms);
+        chummery.setBoysRNumbers(boysRNumbers);
+        chummery.setGirlsRNumbers(girlsRNumbers);
+        chummery.setPhoneNumber(phoneNumber);
+            chummeryRepository.save(chummery);
+        });
+        return "redirect:/student";
+    }
+}
---
+++ dataAO/ChummeryRepository.java
@@ -0,0 +1,7 @@
+package com.example.kursova.dataAO;
+
+import com.example.kursova.entities.Chummery;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChummeryRepository extends JpaRepository<Chummery, Integer> {
+}
