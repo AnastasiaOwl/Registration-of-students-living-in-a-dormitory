8880c60d6b85ed012f313d36fac271f5a8447321
AnastasiaOwl <124703588+AnastasiaOwl@users.noreply.github.com>
2023-11-06 20:22:53 +0200

    services

--- src/main/java/com/example/kursova/entities/Student.java
+++ src/main/java/com/example/kursova/entities/Student.java
@@ -55,7 +55,7 @@ public class Student {
     @ManyToMany(mappedBy = "students")
     private Set<Event> events = new LinkedHashSet<>();
 
-    @ManyToMany
+    @ManyToMany//
     @JoinTable(name = "student_services",
             joinColumns = @JoinColumn(name = "student_id"),
             inverseJoinColumns = @JoinColumn(name = "service_id"))
